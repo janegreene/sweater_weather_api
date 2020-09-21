de2ab6e9112cd79cea3682e80736e71a2700c3a9
Jane Greene <janegreenecasa@gmail.com>
2020-09-21 13:18:59 -0600

    updated test for more details

--- spec/requests/api/v1/climbing_routes_spec.rb
+++ spec/requests/api/v1/climbing_routes_spec.rb
@@ -6,46 +6,19 @@ describe "Climbing Routes API Endpoint for Location" do
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:data][:attributes][:location]).to eq("erwin, tn")
+    expect(json[:data][:attributes][:forecast]).to be_a(Hash)
+    expect(json[:data][:attributes][:routes]).to be_a(Array)
+    expect(json[:data][:attributes][:routes][0][:name]).to be_a(String)
+    expect(json[:data][:attributes][:routes][0][:name]).to_not eq(nil)
+    expect(json[:data][:attributes][:routes][0][:type]).to be_a(String)
+    expect(json[:data][:attributes][:routes][0][:type]).to_not eq(nil)
+    expect(json[:data][:attributes][:routes][0][:rating]).to be_a(String)
+    expect(json[:data][:attributes][:routes][0][:rating]).to_not eq(nil)
+    expect(json[:data][:attributes][:routes][0][:location]).to be_an(Array)
+    expect(json[:data][:attributes][:routes][0][:location]).to_not eq(nil)
+    expect(json[:data][:attributes][:routes][0][:distance_to_route]).to be_a(Float)
+    expect(json[:data][:attributes][:routes][0][:distance_to_route]).to_not eq(nil)
   end
 end
-# https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=40.03&lon=-105.25&maxDistance=10&minDiff=5.6&maxDiff=5.10&key=ENV[trail_api_key]
-# {
-#   "data": {
-#     "id": "null",
-#     "type": "climbing route",
-#     "attributes": {
-#       "location": "erwin,tn",
-#       "forecast": {
-#         "summary": "Raining cats and dogs",
-#         "temperature": "65"
-#       },
-#       "routes": [
-#         {
-#           "name": "Dopey Duck",
-#           "type": "Trad",
-#           "rating": "5.9",
-#           "location": [
-#             "North Carolina",
-#             "2. Northern Mountains Region",
-#             "Linville Gorge",
-#             "Shortoff Mountain"
-#           ]
-#           "distance_to_route": "76.876"
-#         },
-#         {
-#           "name": "Straight and Narrow",
-#           "type": "Trad"
-#           "rating": "5.10a"
-#           "location": [
-#             "North Carolina",
-#             "2. Northern Mountains Region",
-#             "Linville Gorge",
-#             "Shortoff Mountain"
-#           ]
-#           "distance_to_route": "76.876"
-#         },
-#         {...}
-#       ]
-#     }
-#   }
-# }
