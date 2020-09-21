5ad2e94863399e8fadd1f8c85aa824b8ef5450ba
Jane Greene <janegreenecasa@gmail.com>
2020-09-21 10:44:17 -0600

    started serializer still wip

--- app/controllers/api/v1/climbing_routes_controller.rb
+++ app/controllers/api/v1/climbing_routes_controller.rb
@@ -1,13 +1,18 @@
 class Api::V1::ClimbingRoutesController < ApplicationController
   def index
-    lat = 40.03
-    lon = 105.25
+    location = MapService.get_location(params[:location])
+    forecast = ForecastFacade.fetch_forecast(params[:location])
+    # forecast = WeatherService.get_weather(location)
+    lat = location[:results][0][:locations][0][:latLng][:lat]
+    lon = location[:results][0][:locations][0][:latLng][:lng]
     conn = Faraday.new('https://www.mountainproject.com')
     response = conn.get("data/get-routes-for-lat-lon") do |req|
        req.params[:key] = ENV["trail_api_key"]
        req.params[:lat] = lat
        req.params[:lon] = lon
+       req.params[:maxResults] = 10
     end
     require "pry"; binding.pry
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
---
+++ app/serializers/climbing_route_serializer.rb
@@ -0,0 +1,4 @@
+class ClimbingRouteSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :id
+end
