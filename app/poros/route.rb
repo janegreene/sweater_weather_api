3da74bf2f358bc126c24f18d22034224b318da5d
Jane Greene <janegreenecasa@gmail.com>
2020-09-21 13:13:42 -0600

    passing test and it looks like what was perscribed

--- app/poros/route.rb
+++ app/poros/route.rb
@@ -10,13 +10,32 @@ class Route
   end
 
   def set_forecast(params)
-    ForecastFacade.fetch_forecast(params)
+    forecast = Hash.new
+    result = ForecastFacade.fetch_forecast(params)
+    forecast[:summary] = result.current[:weather][0][:description]
+    forecast[:temperature] = result.current[:temp]
+    forecast
   end
 
   def nearby_routes
+    routes = []
     location = MapService.get_location(@location)
     lat = location[:results][0][:locations][0][:latLng][:lat]
     lon = location[:results][0][:locations][0][:latLng][:lng]
-    RouteService.get_routes(lat, lon)
+    route_data = RouteService.get_routes(lat, lon)
+    route_data[:routes].each do |route|
+      route_hash = Hash.new
+      route_hash[:name] = route[:name]
+      route_hash[:type] = route[:type]
+      route_hash[:rating] = route[:rating]
+      route_hash[:location] = route[:location]
+      route_hash[:distance_to_route] = distance(@location, route[:longitude], route[:latitude])
+      routes << route_hash
+    end
+    routes
+  end
+
+  def distance(location, route_lon, route_lat)
+    MapService.get_distance(location, route_lon, route_lat)
   end
 end
--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -5,6 +5,18 @@ class MapService
         faraday.params['location'] = city
         faraday.params['outFormat'] = 'json'
     end
-    JSON.parse(response.body, symbolize_names: true)#[:results][0][:locations][0][:latLng]
+    JSON.parse(response.body, symbolize_names: true)
+  end
+  def self.get_distance(start, toLat, toLon)
+    # final = [toLat, toLon].join(',').to_s
+    # final = [toLat.to_s, toLon.to_s].join(',')
+    final = [toLon.to_s, toLat.to_s].join(',')
+    response = Faraday.get('https://www.mapquestapi.com/directions/v2/route?') do |faraday|
+        faraday.params['key'] = ENV['mapquest_api_key']
+        faraday.params[:from] = start
+        faraday.params[:to] = final
+        end
+        json = JSON.parse(response.body, symbolize_names: true)
+        json[:route][:distance]
   end
 end
