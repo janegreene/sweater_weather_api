f6589691162a5ab0f919d8c2be3fcff8ac63dbe1
Jane Greene <janegreenecasa@gmail.com>
2020-09-22 17:07:52 -0600

    sad path error handling for users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    if params[:password] == params[:password_confirmation]
-      user = User.create(user_params)
+      user = User.new(user_params)
+    if user.save
       render json: UserSerializer.new(user), status: :created
     else
-      render json: "Please try again", status: 400
+      render json: user.errors.full_messages.to_sentence, status: 404
     end
   end
 
