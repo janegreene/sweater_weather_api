a73f4bb1e4393403d9384d2b36ca066d0744f7de
Jane Greene <janegreenecasa@gmail.com>
2020-09-22 20:50:59 -0600

    completed road trip endoint

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,12 +1,11 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    user = User.find_by(trip_params[:api_key])
+    user = User.find_by(api_key: trip_params[:api_key])
     if user
-      #call for maps and stuff
-      #if the trip is legit save it and rend the response
-      render json: RoadTripSerializer.new(user), status: :created
-    # else
-    #   render json: user.errors.full_messages.to_sentence, status: 404
+      trip = RoadTripFacade.make_trip(trip_params)
+      render json: RoadTripSerializer.new(trip), status: :created
+    else
+      render json: { error: 'Invalid Credentials', status: 404 }, status: 404
     end
   end
 
--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -5,6 +5,14 @@ class MapService
         faraday.params['location'] = city
         faraday.params['outFormat'] = 'json'
     end
-    JSON.parse(response.body, symbolize_names: true)#[:results][0][:locations][0][:latLng]
+    JSON.parse(response.body, symbolize_names: true)
+  end
+  def self.get_duration(start, final)
+    response = Faraday.get('https://www.mapquestapi.com/directions/v2/route?') do |faraday|
+      faraday.params['key'] = ENV['mapquest_api_key']
+      faraday.params[:from] = start
+      faraday.params[:to] = final
+    end
+    json = JSON.parse(response.body, symbolize_names: true)
   end
 end
